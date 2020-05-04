221bca7b20ec057cba6a39b6624be3d2961b5c28
Mehedi Hasan <m.mehedi_hasan@hotmail.com>
2020-05-04 21:09:40 +0600

    initial commi

---
+++ IosGenericTableview/IosGenericTableview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IosGenericTableview
+//
+//  Created by Md. Mehedi Hasan on 4/5/20.
+//  Copyright © 2020 Mehedihasan290. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
