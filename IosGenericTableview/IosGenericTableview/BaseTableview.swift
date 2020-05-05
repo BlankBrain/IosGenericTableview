9e6a6ae831a36a50f492fb42cef0a8dabccf9564
Mehedi Hasan <m.mehedi_hasan@hotmail.com>
2020-05-06 05:16:50 +0600

    documentation added

--- IosGenericTableview/IosGenericTableview/BaseTableview.swift
+++ IosGenericTableview/IosGenericTableview/BaseTableview.swift
@@ -9,27 +9,27 @@
 import Foundation
 import UIKit
 
+//MARK: - step 1 : create a tableviewController and point it to the child of this class
+//MARK: - step 2 : supply a array of objects and custom cell type in the viewdidload/viewdidapper  - done
+// this class only provides data showing function of tableview , didselect row method / other methods  would have to implimented in child class if needed
+
+
+
 class BaseTableViewController<T: BaseCell<U> , U>: UITableViewController  {
-    
     var cellId:String = "cell"
     var tableViewItem = [U]()
-    
     override func viewDidLoad() {
            super.viewDidLoad()
         tableView.register(T.self, forCellReuseIdentifier: cellId)
-        
        }
-    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tableViewItem.count
        }
-       
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! BaseCell<U>
         cell.tableViewItem = tableViewItem[indexPath.row]
          return cell
        }
-    
 }
 class BaseCell<U>:UITableViewCell{
        var tableViewItem: U!
--- IosGenericTableview/IosGenericTableview/ChildTableViewController.swift
+++ IosGenericTableview/IosGenericTableview/ChildTableViewController.swift
@@ -7,26 +7,37 @@
 //
 
 import UIKit
-
+//MARK:- use this as a subview to show only a tableview
+// use different file for defining custom cell if needed
+// provide only a array of tableViewItem objects of any custom cell type and you are done
 class ChildTableViewController: BaseTableViewController <dogCell , Dog> {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         tableViewItem = [
                Dog(name: "ranga"),
                Dog(name: "banga"),
                Dog(name: "vutu")
                ]
+    
     }
 
 
 }
+//MARK: - additional tableview functions can be added here if needed
+extension ChildTableViewController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+}
 
-
+// MARK: - object of any type which needs to be shown in the cell
 struct Dog {
        let name:String
    }
 
+//MARK:- custum cell where object needs to be mapped
 class dogCell: BaseCell<Dog>{
        
     override var tableViewItem: Dog!{
